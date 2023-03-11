b97301fce01341ba13ff5ebf75842c1f4b0adea5
silentstorm902 <jozsefkiss90@gmail.com>
2023-03-11 01:19:18 +0100

    fffffff

--- server/server.js
+++ server/server.js
@@ -32,9 +32,8 @@ const port = process.env.PORT || '5000'
 
 const path = require("path");
 
-// Step 1:
 app.use(express.static(path.resolve(__dirname, "../client/build")));
-// Step 2:
+
 app.get("*", function (request, response) {
   response.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
 });
